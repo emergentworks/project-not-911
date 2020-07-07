9f75af73fc2aa747faf0278f2f0302fa1d3f575b
Ross <ross_patton@protonmail.com>
2020-07-07 14:50:30 -0400

    Create Categories.ts
    setup a basic constants file for categories

---
+++ constants/Categories.ts
@@ -0,0 +1,15 @@
+const Categories = [{
+  display: 'Violence'
+}, {
+  display: 'Mental Health Crisis',
+}, {
+  display: 'Homelessness',
+}, {
+  display: 'Noise / Nuisances',
+}, {
+  display: 'Hate Crimes',
+}, {
+  display: 'Substance Abuse',
+}];
+
+export default Categories;
