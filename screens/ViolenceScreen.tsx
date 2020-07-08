64bdaa52d5f5f867feeef9ef0ef2326a7b7433f7
TheTruth312 <60796524+TheTruth312@users.noreply.github.com>
2020-07-08 15:57:52 -0700

    Linked a phone number to Violence page

--- screens/ViolenceScreen.tsx
+++ screens/ViolenceScreen.tsx
@@ -1,17 +1,24 @@
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
+import * as Linking from 'expo-linking';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
 export default function ViolenceScreen() {
-return (
-<View style={styles.container}>
-    <Text style={styles.title}>Violence</Text>
-    <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-    <EditScreenInfo path="/screens/ViolenceScreen.tsx" />
-</View>
-);
+ 
+    return (
+    <View style={styles.container}>
+        <Text style={styles.title}>Violence</Text>
+        <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+        <Text onPress={() => {
+            Linking.openURL('tel:555 555 5555');
+        }}>
+            call number
+        </Text>
+        <EditScreenInfo path="/screens/ViolenceScreen.tsx" />
+    </View>
+    );
 }
 
 const styles = StyleSheet.create({
