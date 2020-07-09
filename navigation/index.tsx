45de6bfeae18f869ff4ce85810c24361e4beb59e
TheTruth312 <60796524+TheTruth312@users.noreply.github.com>
2020-07-09 15:01:11 -0700

    create generic screen component

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -3,16 +3,11 @@ import { createStackNavigator } from '@react-navigation/stack';
 import * as React from 'react';
 import { ColorSchemeName } from 'react-native';
 
-import ViolenceScreen from '../screens/ViolenceScreen';
-import MentalHealthCrisisScreen from '../screens/MentalHealthCrisisScreen';
-import HomelessnessScreen from '../screens/HomelessnessScreen';
-import NoiseNuisancesScreen from '../screens/NoiseNuisancesScreen';
-import HateCrimeScreen from '../screens/HateCrimeScreen';
-import SubstanceAbuseScreen from '../screens/SubstanceAbuseScreen';
 import NotFoundScreen from '../screens/NotFoundScreen';
 import { RootStackParamList } from '../types';
 import BottomTabNavigator from './BottomTabNavigator';
 import LinkingConfiguration from './LinkingConfiguration';
+import PhoneNumberScreen from '../screens/PhoneNumberScreen';
 
 // If you are not familiar with React Navigation, we recommend going through the
 // "Fundamentals" guide: https://reactnavigation.org/docs/getting-started
@@ -34,12 +29,18 @@ function RootNavigator() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Root" component={BottomTabNavigator} />
-      <Stack.Screen name="ViolenceScreen" component={ViolenceScreen}/>
-      <Stack.Screen name="MentalHealthCrisisScreen" component={MentalHealthCrisisScreen}/>
-      <Stack.Screen name="HomelessnessScreen" component={HomelessnessScreen}/>
-      <Stack.Screen name="NoiseNuisancesScreen" component={NoiseNuisancesScreen}/>
-      <Stack.Screen name="HateCrimeScreen" component={HateCrimeScreen}/>
-      <Stack.Screen name="SubstanceAbuseScreen" component={SubstanceAbuseScreen}/>
+      <Stack.Screen name="ViolenceScreen"
+        component={PhoneNumberScreen}/>
+      <Stack.Screen name="MentalHealthCrisisScreen"
+        component={PhoneNumberScreen}/>
+      <Stack.Screen name="HomelessnessScreen"
+        component={PhoneNumberScreen}/>
+      <Stack.Screen name="NoiseNuisancesScreen"
+        component={PhoneNumberScreen}/>
+      <Stack.Screen name="HateCrimeScreen"
+        component={PhoneNumberScreen}/>
+      <Stack.Screen name="SubstanceAbuseScreen"
+        component={PhoneNumberScreen}/>
       <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
     </Stack.Navigator>
   );
