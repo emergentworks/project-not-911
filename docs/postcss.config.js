bf09c770b135a70ce5360a77bce3ac5f6162c445
Ross <rossPatton@users.noreply.github.com>
2020-07-14 11:23:34 -0400

    rearrange things a bit. add shortcut start command. fix tailwind setup (#5)

--- docs/postcss.config.js
+++ docs/postcss.config.js
@@ -1,20 +1,19 @@
 const jekyllEnv = process.env.JEKYLL_ENV || "development";
 
 module.exports = {
-    plugins: [
+  plugins: [
     require("postcss-import"),
     require("tailwindcss")("./tailwind.config.js"),
     require("autoprefixer"),
     ...(jekyllEnv !== "development"
     ? [
         require("@fullhuman/postcss-purgecss")({
-            content: ["!(_site|node_modules)/**/*.+(html|js|md)", "*.html"],
-            whitelistPatternsChildren: [/highlight/],
-            defaultExtractor: (content) =>
-            content.match(/[\w-/:]+(?<!:)/g) || [],
+          content: ["!(_site|node_modules)/**/*.+(html|js|md)", "*.html"],
+          defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+          whitelistPatternsChildren: [/highlight/],
         }),
         require("cssnano")({ preset: "default" }),
-        ]
+      ]
     : [])
-]
+  ]
 };
