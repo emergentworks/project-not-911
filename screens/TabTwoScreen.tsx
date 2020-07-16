7342ed9bee2d2cf2236169bb16bd1c7db1fb3594
TheTruth312 <60796524+TheTruth312@users.noreply.github.com>
2020-07-16 09:16:37 -0700

    started work on about page (#7)
    * started work on about page
    
    * switched to dark theme and worked on about page
    
    * added unorderedlist package

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,14 +1,32 @@
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
+import Unorderedlist from 'react-native-unordered-list';
 
-import { EditScreenInfo, Text, View } from '../components';
+import { Text, View } from '../components';
 
 export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
+      {/* <Text style={[styles.marginBottom30, styles.title]}>Defunding the Police</Text> */}
+      <Text style={[styles.marginBottom30, styles.title]}>Public Safety First</Text>
+      
+      <Unorderedlist color="white" >
+  <Text style={styles.paragraph}>Connecting people to trusted support communities.</Text>
+      </Unorderedlist>
+      <Unorderedlist color='white'>
+      <Text style={styles.paragraph}>Protect the Vulnerable communities (from police).</Text>
+      </Unorderedlist>
+
+      <Text style={[styles.marginBottom30, styles.title]}>Part of a bigger initiative Code</Text>
+      <Unorderedlist color="white">
+      <Text style={styles.paragraph}>Step towards police defunding & Abolition</Text>
+      </Unorderedlist>
+      <Unorderedlist color="white">
+      <Text style={styles.paragraph}>Decriminalizing unwanted community activity.</Text>
+      </Unorderedlist>
+      <Unorderedlist color="white">
+      <Text style={styles.paragraph}>Moving away from police as the default resource.</Text>
+      </Unorderedlist>
     </View>
   );
 }
@@ -16,16 +34,25 @@ export default function TabTwoScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    backgroundColor: 'blue',
+    padding: 20,
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    color: 'white',
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  paragraph: {
+    fontSize: 15,
+    color: 'white',
+    // paddingTop: 10,
+    // paddingLeft: 22,
+    // paddingBottom: 35,
+    marginBottom: 20,
+  },
+  marginBottom30: {
+    marginBottom: 30,
   },
 });
