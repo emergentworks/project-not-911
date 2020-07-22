b240ccc863053cee99ba074822436983aeeac6fa
TheTruth312 <60796524+TheTruth312@users.noreply.github.com>
2020-07-22 09:29:21 -0700

    hotline light mode styles (#22)

--- screens/PhoneNumberListScreen/components/PhoneNumberListLight.tsx
+++ screens/PhoneNumberListScreen/components/PhoneNumberListLight.tsx
@@ -3,33 +3,44 @@ import { ScrollView, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import * as Linking from 'expo-linking';
 
 import { Text, View } from '../../../components';
-import { PhoneBlue } from '../../../svgs';
+import { PhoneWhite, PhoneBlue, PhoneOrange, CaretWhite } from '../../../svgs';
 import { ComponentProps } from '../types';
+import { Styles } from '../../../constants';
 
 export const PhoneNumberListLight = (props: ComponentProps) => {
   const {numbers} = props;
-
+console.log('light')
   return (
-    <View style={styles.container}>
+    <View
+      lightColor={Styles.white}
+      style={[styles.container, styles.paddingTop30]}>
       <ScrollView showsVerticalScrollIndicator={false}>
         {numbers instanceof Array
           && numbers.map((entry: any, i) => (
             <View key={i} style={styles.container}>
+              <Text style={[styles.hours && styles.hours,
+              styles.bold,
+              entry.crisis && {color: Styles.red}]}>
+                {entry.hours}
+              </Text>
               <Text style={[styles.bold, styles.title]}>
                 {entry.display}
               </Text>
+              <Text style={styles.tel}>
+                {entry.tel}
+              </Text>
               <TouchableOpacity
-                style={styles.phoneWrap}
+                style={[styles.phoneWrap, entry.crisis && {backgroundColor: Styles.red}]}
                 onPress={() => {
                   Linking.openURL(`tel:${entry.tel}`);
                 }}>
-                <PhoneBlue />
+                <PhoneWhite />
               </TouchableOpacity>
-              <View
-                style={styles.separator}
-                lightColor="#eee"
-                darkColor="rgba(255,255,255,0.1)"
-              />
+                <View
+                  style={styles.separator}
+                  lightColor="#eee"
+                  darkColor="#CFCFCF"
+                />
             </View>
           ))}
       </ScrollView>
@@ -41,12 +52,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     flex: 1,
     justifyContent: 'center',
-    textAlign: 'center'
+    textAlign: 'center',
+    backgroundColor: Styles.white,
+  },
+  paddingTop30: {
+    paddingTop: 30,
   },
   title: {
     fontSize: 24,
-    fontWeight: 'bold',
     width: '80%',
+    paddingBottom: 10,
+    color: Styles.black,
   },
   separator: {
     height: 1,
@@ -64,19 +80,27 @@ const styles = StyleSheet.create({
     marginBottom: 30,
   },
   hours: {
-    color: 'red',
-    paddingTop: 20,
-    paddingBottom: 20,
+    color: Styles.blue,
+    paddingBottom: 5,
   },
   phoneWrap: {
     height: 50,
     width: 50,
-    backgroundColor: 'blue',
+    backgroundColor: Styles.blue,
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
     marginTop: 25,
     borderRadius: 1000,
     padding: 10,
+  },
+  crisis: {
+    padding: 30,
+    backgroundColor: Styles.red,
+    borderRadius: 10,
+    marginBottom: 30,
+  },
+  tel: {
+    color: Styles.black,
   },
 });
