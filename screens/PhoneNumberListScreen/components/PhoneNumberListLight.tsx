176974b9eda725a1365183c282ef3c958ea38a4c
Ross <rossPatton@users.noreply.github.com>
2020-07-23 11:18:37 -0400

    update light version of hotline page. fix circles. double check on native devices (#24)

--- screens/PhoneNumberListScreen/components/PhoneNumberListLight.tsx
+++ screens/PhoneNumberListScreen/components/PhoneNumberListLight.tsx
@@ -1,46 +1,52 @@
 import * as React from 'react';
-import { ScrollView, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
 import * as Linking from 'expo-linking';
 
 import { Text, View } from '../../../components';
-import { PhoneWhite, PhoneBlue, PhoneOrange, CaretWhite } from '../../../svgs';
+import { PhoneWhite } from '../../../svgs';
 import { ComponentProps } from '../types';
 import { Styles } from '../../../constants';
 
 export const PhoneNumberListLight = (props: ComponentProps) => {
   const {numbers} = props;
-console.log('light')
+
   return (
     <View
       lightColor={Styles.white}
-      style={[styles.container, styles.paddingTop30]}>
+      style={styles.container}>
       <ScrollView showsVerticalScrollIndicator={false}>
         {numbers instanceof Array
           && numbers.map((entry: any, i) => (
-            <View key={i} style={styles.container}>
-              <Text style={[styles.hours && styles.hours,
-              styles.bold,
-              entry.crisis && {color: Styles.red}]}>
+            <View key={i}>
+              <Text
+                style={[
+                  styles.hours,
+                  styles.bold,
+                  styles.centerTxt,
+                  entry.crisis && {color: Styles.orange},
+                ]}>
                 {entry.hours}
               </Text>
-              <Text style={[styles.bold, styles.title]}>
+              <Text style={[styles.bold, styles.centerTxt, styles.title]}>
                 {entry.display}
               </Text>
-              <Text style={styles.tel}>
+              <Text style={[styles.centerTxt, styles.tel]}>
                 {entry.tel}
               </Text>
               <TouchableOpacity
-                style={[styles.phoneWrap, entry.crisis && {backgroundColor: Styles.red}]}
+                style={[
+                  styles.phoneWrap,
+                  entry.crisis && {backgroundColor: Styles.orange}
+                ]}
                 onPress={() => {
                   Linking.openURL(`tel:${entry.tel}`);
                 }}>
                 <PhoneWhite />
               </TouchableOpacity>
-                <View
-                  style={styles.separator}
-                  lightColor="#eee"
-                  darkColor="#CFCFCF"
-                />
+              <View
+                style={styles.separator}
+                lightColor="#cfcfcf"
+              />
             </View>
           ))}
       </ScrollView>
@@ -49,23 +55,25 @@ console.log('light')
 }
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
     flex: 1,
-    justifyContent: 'center',
+    paddingTop: 60,
+  },
+  centerTxt: {
     textAlign: 'center',
-    backgroundColor: Styles.white,
   },
   paddingTop30: {
     paddingTop: 30,
   },
   title: {
-    fontSize: 24,
-    width: '80%',
-    paddingBottom: 10,
     color: Styles.black,
+    fontSize: 24,
+    marginHorizontal: 'auto',
+    paddingHorizontal: 20,
+    paddingBottom: 10,
   },
   separator: {
     height: 1,
+    marginHorizontal: 'auto',
     marginVertical: 30,
     width: '80%',
   },
@@ -84,23 +92,18 @@ const styles = StyleSheet.create({
     paddingBottom: 5,
   },
   phoneWrap: {
-    height: 50,
-    width: 50,
-    backgroundColor: Styles.blue,
-    flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 25,
+    alignSelf: 'center',
+    backgroundColor: Styles.blue,
     borderRadius: 1000,
-    padding: 10,
-  },
-  crisis: {
-    padding: 30,
-    backgroundColor: Styles.red,
-    borderRadius: 10,
-    marginBottom: 30,
+    flex: 1,
+    height: 70,
+    justifyContent: 'center',
+    padding: 17,
+    width: 70,
   },
   tel: {
     color: Styles.black,
+    marginBottom: 15,
   },
 });
