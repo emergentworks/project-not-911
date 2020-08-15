1b613c57d83f9de3aa40ddd47cd7cf65bd6d8843
Ross <ross_patton@protonmail.com>
2020-08-14 22:40:44 -0400

    stealth deploy web app. no public link, just testing

---
+++ docs/app/precache-manifest.b649f380020499e4e0d2678dcd63163d.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = [
+  {
+    "revision": "4aa46ae9ea8978def887",
+    "url": "/app/static/js/app.f55c9b25.chunk.js"
+  },
+  {
+    "revision": "64fa760af13884d283de",
+    "url": "/app/static/js/runtime~app.841dc5b3.js"
+  },
+  {
+    "revision": "41bebff16fcf2b780761",
+    "url": "/app/static/js/2.feead362.chunk.js"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/app/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "8f6b453eb030549471bd0d95f3b60542",
+    "url": "/app/./fonts/Inter_100Thin.ttf"
+  },
+  {
+    "revision": "b77a98479d88ab0df8f14a94a542a033",
+    "url": "/app/./fonts/Inter_200ExtraLight.ttf"
+  },
+  {
+    "revision": "5a8cd28ecea40464653def6869c41cbc",
+    "url": "/app/./fonts/Inter_300Light.ttf"
+  },
+  {
+    "revision": "56786aa1952d68dada47626302897eae",
+    "url": "/app/./fonts/Inter_400Regular.ttf"
+  },
+  {
+    "revision": "ddc03dcdbfb32329aa419994ff329189",
+    "url": "/app/./fonts/Inter_500Medium.ttf"
+  },
+  {
+    "revision": "65cbb7515961a8f823110c2a612fd0d9",
+    "url": "/app/./fonts/Inter_600SemiBold.ttf"
+  },
+  {
+    "revision": "c73899dff65a846e3d8423b04cb041ec",
+    "url": "/app/./fonts/Inter_700Bold.ttf"
+  },
+  {
+    "revision": "c88d8bbbf91fb59ecb13ac80d7bd56eb",
+    "url": "/app/./fonts/Inter_800ExtraBold.ttf"
+  },
+  {
+    "revision": "de70a8a9ce5dcf6f9e5046ad0af9abac",
+    "url": "/app/./fonts/Inter_900Black.ttf"
+  },
+  {
+    "revision": "49a79d66bdea2debf1832bf4d7aca127",
+    "url": "/app/./fonts/SpaceMono-Regular.ttf"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/app/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/app/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "ca9ce9ff0676a9b04ef0f8a3ad17dd08",
+    "url": "/app/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/app/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "c39278f7abfc798a241551194f55e29f",
+    "url": "/app/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "b70cea0339374107969eb53e5b1f603f",
+    "url": "/app/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "b49ae8ab2dbccb02c4d11caaacf09eab",
+    "url": "/app/./fonts/Fontisto.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/app/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "3c851d60ad5ef3f2fe43ebd263490d78",
+    "url": "/app/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/app/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/app/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/app/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/app/expo-service-worker.js"
+  },
+  {
+    "revision": "f8092b4f5f96648e0ec4eb0433b8d29d",
+    "url": "/app/favicon-16.png"
+  },
+  {
+    "revision": "e321744c431fad0ebcd88ccaa0c7ef02",
+    "url": "/app/favicon-32.png"
+  },
+  {
+    "revision": "9e6c2b1bb74ae35d12969aa4d8a6dc72",
+    "url": "/app/favicon.ico"
+  },
+  {
+    "revision": "faed1ef8b84cfffe623b474d8f23d26a",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "e72f3a475108e7b5e44259d214d9f32b",
+    "url": "/app/manifest.json"
+  },
+  {
+    "revision": "f1bf9c70bb653dbd848104cfc21f79ef",
+    "url": "/app/register-service-worker.js"
+  }
+];
