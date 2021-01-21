1f8f78d5ef0db50eb9ad1d6fe0fc459e9df1ec71
ShotofADRENALIN <65616933+ShotofADRENALIN@users.noreply.github.com>
2021-01-21 14:40:31 -0500

    Updated phone numbers (#52)
    Co-authored-by: Elijah Johnson <shotofadrenalin@Elijahs-MBP.lan>

--- constants/PhoneNumbers.ts
+++ constants/PhoneNumbers.ts
@@ -9,6 +9,9 @@ type Props = {
     text?: { content?: string; number: string; },
     website?: string,
     voicemail?: true,
+    description?: string,
+    borough?: string,
+    note?: string,
   }[];
 };
 
@@ -47,12 +50,18 @@ export const PhoneNumbers: Props = {
     tel: '1-800-664-5880',
     hours: '24 Hour Access',
   }, {
+      display: "Barrier Free Living",
+      description: "Outside of these hours call the National Domestic Violence Hotline", 
+      tel: "1 (800) 799-7233",
+      website: "https://www.bflnyc.org/"
+    },
+    {
     crisis: true,
     display: 'National Domestic Violence Hotline',
     tel: '1-800-799-7233',
     text: { content: 'LOVEIS', number: '1-866-331-9474' },
     hours: '24 Hour Access',
-  }],
+    }],
 
   mentalhealth: [{
     crisis: true,
@@ -90,6 +99,13 @@ export const PhoneNumbers: Props = {
     tel: '1-800-985-5990',
     text: {content: 'TalkWithUs or Hablanos', number: '66746'},
     website: 'https://www.samhsa.gov/find-help/disaster-distress-helpline',
+  },
+  {
+    display: "The Samaritans",
+    description: "24-Hour Crisis Hotline. Samaritans free, confidential, 24-hour emotional support and crisis response hotline service is available on an immediate and ongoing basis to help people who are dealing with every kind of problem, illness, trauma or loss as they try to cope with their difficulties.The hotline provides those in crisis (as well as the people who care for them) with a 24-hour safety net that can be used to fill-in service gaps, bridge between appointments and act as a source of ongoing emotional maintenance for those with chronic emotional issues.",
+    tel: "(212) 673-3000",
+    hours: '24 Hour Access',
+    website: "https://samaritansnyc.org/24-hour-crisis-hotline/"
   }],
 
   immigration: [{
@@ -137,6 +153,29 @@ export const PhoneNumbers: Props = {
     ],
     tel: '212-941-9090',
     website: 'https://door.org/programs-services/runaway-homeless-youth/',
+  }, {
+    display: "Ali Forney Center",
+    is24Hr: true,
+    description: "The Ali Forney Center is the largest and most comprehensive organization in the nation dedicated to housing LGBT youth. AFC serves youth aged 16-24, and provides emergency and transitional housing, street outreach, a drop-in center, medical and mental health treatment, a vocational/educational program, and a counseling project for the families of LGBT youth. ",
+    borough: "Manhattan/Brooklyn",
+    tel: "(212) 206-0574",
+    website: "https://www.aliforneycenter.org/"
+  },
+  {
+    display: "Bowery Residents Committee Homeless Services Hotline",
+    tel: "(212) 533-5151",
+    website: "www.brc.org"
+  },
+  {
+    display: "Coalition for the Homeless Crisis Intervention Program",
+    tel: "(212) 776-2177",
+    website: "https://www.coalitionforthehomeless.org/our-programs/crisis-services/crisis-intervention/",
+    note: "*Please note response may be limited due to COVID-19"
+  },
+  {
+    display: "Sylvia’s Place",
+    description: "Sylvia’s Place is an emergency night shelter for self identified Gay, Lesbian, Bi-sexual, trans-gendered, trans-sexual and questioning youth from 16 to 23 years of age.",
+    tel: "(212) 629-7440"
   }],
 
   legal: [{
@@ -159,7 +198,8 @@ export const PhoneNumbers: Props = {
     hours: '24 Hour Access',
     tags: ['Counseling', 'Assault by NYPD'],
     tel: '1-866-689-4357',
-  }],
+  }
+],
 
   poison: [{
     hours: '24 Hour Access',
@@ -171,7 +211,13 @@ export const PhoneNumbers: Props = {
       'Registered Nurses & Pharmacists',
     ],
     tel: '212-764-7667',
-  }, {
+  },
+    {
+    display: "Crisis Services for People Struggling with Addiction",
+    tel: "(212) 763-0596",
+    website: "https://www.projectrenewal.org/need-help"
+  }, 
+  {
     hours: '24 Hour Access',
     display: 'SAMHSA (Substance Abuse and Mental Health Services Administration) National Helpline',
     tags: [
@@ -180,7 +226,15 @@ export const PhoneNumbers: Props = {
       'General Information',
     ],
     tel: '1-800-662-4357',
-  }, {
+  },
+    {
+      display: "Bowery Residents Committee Chemical Dependency Crisis Center (Intake Line)",
+      description: "BRC’s Chemical Dependency Crisis Center (CDCC) provides 32 beds of supportive inpatient and residential substance abuse services for homeless men and women",
+      tel: "(212) 533-3281",
+      website: "https://www.brc.org/chemical-dependency-crisis-center"
+    },
+  
+  {
     crisis: true,
     display: 'NYC Well',
     hours: '24 Hour Access',
@@ -211,4 +265,4 @@ export const PhoneNumbers: Props = {
     tel: '1-877-846-7369',
     text: {number: 'HOPENY'},
   }],
-};
+}
