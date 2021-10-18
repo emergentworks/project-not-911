a0cbf618c67eb3d03b21bf08f3506a4c8799cad8
Ross <rossPatton@users.noreply.github.com>
2021-10-17 20:07:51 -0400

    add support for descriptions and multiple numbers per item (#85)
    * add support for descriptions and multiple numbers per item
    
    * little extra safety check

---
+++ screens/index.ts
@@ -0,0 +1,6 @@
+export { AboutScreen } from './AboutScreen';
+export { CategoryScreen } from './CategoryScreen';
+export { CitySelectScreen } from './CitySelectScreen';
+export { NotFoundScreen } from './NotFoundScreen';
+export { PhoneNumberListScreen } from './PhoneNumberListScreen';
+export { SettingsScreen } from './SettingsScreen';
--- types.tsx
+++ types.tsx
@@ -1,7 +1,7 @@
 import { RouteProp } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 
-export type tCacheUnion = 'Boston' | 'Chicago' | 'New York' | 'San Francisco Bay Area';
+export type tCacheUnion = 'Boston' | 'Chicago' | 'New York' | 'Los Angeles' | 'San Francisco Bay Area';
 
 /**
  * @description global types file. should just live in a declarations folder, ideally
